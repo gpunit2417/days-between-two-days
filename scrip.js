d32fa653644e332748cac9502dd570fac11ac6d3
gpunit2417 <punitgoyal106@gmail.com>
2024-09-28 11:32:26 +0530

    Days Counter Frontend Project

---
+++ scrip.js
@@ -0,0 +1,17 @@
+function calculateDays() {
+    const date1 = document.getElementById('date1').value;
+    const date2 = document.getElementById('date2').value;
+
+    if (date1 === '' || date2 === '') {
+        document.getElementById('result').innerText = 'Please select both dates.';
+        return;
+    }
+
+    const firstDate = new Date(date1);
+    const secondDate = new Date(date2);
+
+    const timeDifference = Math.abs(secondDate - firstDate);
+    const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
+    document.getElementById('result').innerText = `There are ${daysDifference} days between ${date1} and ${date2}.`;
+}
